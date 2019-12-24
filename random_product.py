efd595c4227b9fe6a4b032925c0da1669d532031
Anongnat R <anongnat.rat@ku.th>
2019-12-24 15:23:12 +0700

    remove pandas

--- random_product.py
+++ random_product.py
@@ -1,19 +1,23 @@
-import pandas as pd
 from random import randint
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-df = pd.read_json('product.json')
+product=open('products.csv').read().splitlines()
+table=[i.split(',') for i in product]
+
 app = Flask(__name__)
 # Enable CORS
 CORS(app)
+def get_index(min_p,max_p):
+    i_min = min([i for i in range(len(product)) if float(table[i][1])>=min_p])
+    i_max = max([j for j in range(len(product)) if float(table[j][1])<=max_p])
+    return i_min,i_max
 
 def get_product(index): 
-    # print(index)
-    pd_name = df['product'][index]['name']
-    pd_price = df['product'][index]['price']
-    pd_img = df['product'][index]['img']
-    pd_link = df['product'][index]['url']
+    pd_name = table[index][0]
+    pd_price = table[index][1]
+    pd_img = table[index][2]
+    pd_link = table[index][3]
     return pd_name,pd_price,pd_img,pd_link
 
 @app.route("/randomgift", methods=["POST"])
@@ -21,8 +25,7 @@ def predict():
     if request.method=="POST":
         input_min = request.json["min"]
         input_max = request.json["max"]
-        i_min = min([i for i in range(len(df['product'].index)) if float(df['product'][i]['price'])>=input_min])
-        i_max = max([j for j in range(len(df['product'].index)) if float(df['product'][j]['price'])<=input_max])
+        i_min,i_max=get_index(input_min,input_max)
         index = randint(i_min,i_max)
         pd_name,pd_price,pd_img,pd_link=get_product(index)
     return jsonify(name=pd_name,price=pd_price,image=pd_img,url=pd_link),201
